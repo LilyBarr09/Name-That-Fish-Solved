10b8e7e360a53adaa384818ac62cb7b13994d03d
Liliana Barrera <barrera.jimnlil@gmail.com>
2023-08-16 17:05:29 -0400

    class app comp not moving in indexes

--- src/Components/Class/ClassApp.jsx
+++ src/Components/Class/ClassApp.jsx
@@ -29,7 +29,7 @@ export class ClassApp extends Component {
     incorrectTally: 0,
   };
 
-  total = () this.state.correctTally + this.state.incorrectTally;
+  total = this.state.correctTally + this.state.incorrectTally;
 
   fishGuess = (fish) => {
     const arrValuesToAdd =
