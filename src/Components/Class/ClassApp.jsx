112bf14f6eaa1ac873509c696b5d4452c0c36fcb
Liliana Barrera <barrera.jimnlil@gmail.com>
2023-08-16 15:44:44 -0400

    debugged class comps, showing on screen, now have to debugg fish images changing with guesses

--- src/Components/Class/ClassApp.jsx
+++ src/Components/Class/ClassApp.jsx
@@ -2,20 +2,73 @@ import { Component } from "react";
 import { ClassScoreBoard } from "./ClassScoreBoard";
 import { ClassGameBoard } from "./ClassGameBoard";
 import { ClassFinalScore } from "./ClassFinalScore";
+import { Images } from "../../assets/Images";
+
+const initialFishes = [
+  {
+    name: "trout",
+    url: Images.trout,
+  },
+  {
+    name: "salmon",
+    url: Images.salmon,
+  },
+  {
+    name: "tuna",
+    url: Images.tuna,
+  },
+  {
+    name: "shark",
+    url: Images.shark,
+  },
+];
 
 export class ClassApp extends Component {
   state = {
-    incorrectCount: 0,
-    correctCount: 0,
+    correctTally: 0,
+    incorrectTally: 0,
   };
+
+  fishIndex = this.state.correctTally + this.state.incorrectTally;
+
+  fishGuess = (fish) => {
+    const arrValuesToAdd =
+      fish === initialFishes[this.fishIndex].name ? [1, 0] : [0, 1];
+    this.setState({
+      correctTally: this.state.correctTally + arrValuesToAdd[0],
+      incorrectTally: this.state.incorrectTally + arrValuesToAdd[1],
+    });
+  };
+
+  answersLeft = initialFishes
+    .slice(this.fishIndex, initialFishes.length)
+    .map((fish) => fish.name);
+
   render() {
+    const { correctTally, incorrectTally } = this.state;
+
     return (
       <>
-        <>
-          <ClassScoreBoard />
-          <ClassGameBoard />
-        </>
-        {false && <ClassFinalScore />}
+        {this.fishIndex < 4 && (
+          <>
+            <ClassScoreBoard
+              correctTally={correctTally}
+              incorrectTally={incorrectTally}
+              answers={this.answersLeft}
+            />
+            <ClassGameBoard
+              fishes={initialFishes}
+              fishGuess={this.fishGuess}
+              fishIndex={this.fishIndex}
+            />
+          </>
+        )}
+        {this.fishIndex === 4 && (
+          <ClassFinalScore
+            correctTally={correctTally}
+            totalTally={initialFishes.length}
+          />
+        )}
       </>
     );
   }
--- src/Components/Class/ClassGameBoard.jsx
+++ src/Components/Class/ClassGameBoard.jsx
@@ -1,37 +1,37 @@
 import { Component } from "react";
 import "./styles/game-board.css";
-import { Images } from "../../assets/Images";
-
-const initialFishes = [
-  {
-    name: "trout",
-    url: Images.trout,
-  },
-  {
-    name: "salmon",
-    url: Images.salmon,
-  },
-  {
-    name: "tuna",
-    url: Images.tuna,
-  },
-  {
-    name: "shark",
-    url: Images.shark,
-  },
-];
 
 export class ClassGameBoard extends Component {
+  state = {
+    usersInput: "",
+  };
+  submitForm = (e) => {
+    e.preventDefault();
+    this.props.fishGuess(this.state.usersInput);
+    this.setState({ usersInput: "" });
+  };
+
   render() {
-    const nextFishToName = initialFishes[0];
+    const { usersInput } = this.state;
+
+    const nextFishToName =
+      this.props.fishes[this.props.fishIndex !== 4 ? this.props.fishIndex : 0];
+
     return (
       <div id="game-board">
         <div id="fish-container">
           <img src={nextFishToName.url} alt={nextFishToName.name} />
         </div>
-        <form id="fish-guess-form">
+        <form id="fish-guess-form" onSubmit={this.submitForm}>
           <label htmlFor="fish-guess">What kind of fish is this?</label>
-          <input type="text" name="fish-guess" />
+          <input
+            type="text"
+            name="fish-guess"
+            value={usersInput}
+            onChange={(e) => {
+              this.setState({ usersInput: e.target.value });
+            }}
+          />
           <input type="submit" />
         </form>
       </div>
